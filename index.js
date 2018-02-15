032da87d4b328436c155ede76f3257b414d35c65
Alex <alex.bunardzic@phsa.ca>
2018-02-15 11:45:26 -0800

    adds signout endpoint skeleton

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 //
-const send = require('micro');
+const { json, send } = require('micro');
+const { router, get } = require('microrouter');
+const { postrouter, post } = require('microrouter');
+const auditLogger = require('./user_audit/audit-logger');
+const enumerateObjectProperties = require('./lib/enumerate-object-properties');
 
-module.exports = (req, res) => {
-  res.end('Microservice here!');
+const signin = (req, res) => {
+  send(res, 200, `Hi there ${req.params.who}`);
 }
+
+const signout = async (req, res) => {
+  const body = await json(req);
+  const args = {userId: body.userId, userName: body.userName, ipAddress: body.userIp};
+  prop = enumerateObjectProperties.findProperty(auditLogger.signoutEventLog(args));
+  send(res, 201, `Hi there ${prop}`);
+}
+
+const notfound = (req, res) => {
+  send(res, 400, 'Route not found');
+}
+
+module.exports = router(
+  get('/signin/:who', signin),
+  get('/*', notfound)
+);
+
+module.exports = router(
+  post('/signout', signout)
+);
+
+//module.exports = async(req, res) => {
+//  const data = await json(req);
+//  console.log(data);
+//
+//  return 'Data logged to your console';
+//}
